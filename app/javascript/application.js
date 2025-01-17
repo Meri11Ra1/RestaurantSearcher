c797a0bf94bc0fc6cd561b28f3de209f6755f7d0
Meri11Ra1 <R.t.00293794@gmail.com>
2025-01-17 22:50:11 +0900

    geolocation apiをrails内でテスト

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
 import "controllers"
+import "@hotwired/turbo-rails"
+import { Application } from "@hotwired/stimulus";
+import GeolocationController from "./controllers/geolocation_controller";
+
+const application = Application.start();
+application.register("geolocation", GeolocationController);
