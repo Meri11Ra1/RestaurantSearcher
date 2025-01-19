0a520254650716f73938575ee8a264ba27af7260
Meri11Ra1 <r.t.00293794@gmail.com>
2025-01-19 21:07:33 +0900

    gourmet_apiリクエストの切り分け

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def gourmet_api_request(_parsed_uri) {
+    # Net::HTTP.newでHTTPのクライアントのオブジェクトを作成
+    # Net::HTTP.newでHTTPのクライアントのオブジェクトを作成
+    http = Net::HTTP.new(parsed_uri.host, parsed_uri.port)
+    #HTTPでSSL/TLSを使うかどうかを設定する。
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(parsed_uri.request_uri)
+
+    return http.request(request)
+  }
 end
